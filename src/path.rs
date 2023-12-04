77658d381eea8517a0bda9105b7b2212eed7f7bf
Joshua Rose <joshuarose@gmx.com>
2023-12-04 20:12:01 +1100

    fix: silly typo
    * hopefully this commit transaction is rescinded in later branch refactoring

--- src/path.rs
+++ src/path.rs
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/! contains path-related traits and structs
+//! contains path-related traits and structs
 
 // TODO: add 'OSTYPE' variable to config loader
 
